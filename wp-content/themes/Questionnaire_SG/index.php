d70a55276d66364c9f0518d5174ca4a5320e605b
kevinhuang78 <kevin_huang_iim@yahoo.com>
2016-11-23 20:45:53 +0100

    Ajout de la fonction 'Flash' ainsi que 'Bingo' et le compteur de point, manque la fonction 'Cash'

--- wp-content/themes/Questionnaire_SG/index.php
+++ wp-content/themes/Questionnaire_SG/index.php
@@ -5,23 +5,43 @@
 
 <form action="" method="">
  
+ <div class="col-md-12 score_container">Votre score est de <strong id="score"></strong></div>
 	<div class="container" id="question1">
 	    <div class="question col-md-12">
 	      <div class="texte_container col-md-5">
 	        <h2>Question 1</h2>
+	          <div id="question_simple_01">Question plus simple</div>
 	          <p>Intitulé de la question :</p>
+	          <div id="Crono"></div>
 	          <p><input type="radio" id="r1" name="q1">Réponse 1 (vraie)</p>
 	          <p><input type="radio" id="r2" name="q1">Réponse 2 (fausse)</p>
 	          <p><input type="radio" id="r3" name="q1">Réponse 3 (fausse)</p>
+	          <p><input type="radio" id="i1" name="q1">Intrus (vraie)</p>
+	          <p><input type="radio" id="i2" name="q1">Texte (fausse)</p>
+	          <p><input type="radio" id="i3" name="q1">Texte (fausse)</p>
+	          <div id="indice_01" class="indices">Indice</div>
 	          <a href="#" class="button_answer" id="a1">Confirmer</a>
+	          <a href="#" class="button_answer" id="ai1">Confirmer</a>
 	      </div>
 	      <div class="col-md-7 answer_img_container">
 	      	<img class="question_img" id="img1" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/FAQ.png" alt="Question" />
 	      	<img class="answer_img" id="img2" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/Check.png" alt="Bonne réponse" />
 	      	<img class="answer_img" id="img3" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/Check_wrong.png" alt="Mauvaise réponse" />
 	      </div>
+	      <div class="col-md-12 choix_joker" id="choix_joker_01">
+	      	<div class="col-md-4">
+	      		<a href="#" title="Cash" id="cash_01" onclick="decompte();">Cash</a>
+	      	</div>
+	      	<div class="col-md-4">
+	      		<a href="#" title="Flash" id="flash_01">Flash</a>
+	      	</div>
+	      	<div class="col-md-4">
+	      		<a href="#" title="Bingo" id="bingo_01">Bingo</a>
+	      	</div>
+	      </div>
 	      <div id="answer1" class="col-md-12 answers text-center">
-	      	<p class="reponse" id="reponse1">La réponse rédigée</p>
+	      	<p class="reponse" id="reponse1i">La réponse rédigée pour Flash</p>
+	      	<p class="reponse" id="reponse1">La réponse rédigée pour Bingo</p>
 	      </div>
 	      <div id="next1" class="col-md-12 text-center next">
 	      	<a href="#" class="button_answer" id="n1">Suivant</a>
@@ -33,19 +53,37 @@
 	    <div class="question col-md-12">
 	      <div class="texte_container col-md-5">
 	        <h2>Question 2</h2>
+	          <div id="question_simple_02">Question plus simple</div>
 	          <p>Intitulé de la question :</p>
-	          <p><input type="radio" id="r4" name="q1">Réponse 1 (fausse)</p>
-	          <p><input type="radio" id="r5" name="q1">Réponse 2 (vraie)</p>
-	          <p><input type="radio" id="r6" name="q1">Réponse 3 (fausse)</p>
+	          <p><input type="radio" id="r4" name="q2">Réponse 1 (fausse)</p>
+	          <p><input type="radio" id="r5" name="q2">Réponse 2 (vraie)</p>
+	          <p><input type="radio" id="r6" name="q2">Réponse 3 (fausse)</p>
+	          <p><input type="radio" id="i4" name="q2">Texte (fausse)</p>
+	          <p><input type="radio" id="i5" name="q2">Intrus (vraie)</p>
+	          <p><input type="radio" id="i6" name="q2">Texte (fausse)</p>
+	          <div id="indice_02" class="indices">Indice</div>
 	          <a href="#" class="button_answer" id="a2">Confirmer</a>
+	          <a href="#" class="button_answer" id="ai2">Confirmer</a>
 	      </div>
 	      <div class="col-md-7 answer_img_container">
 	      	<img class="question_img" id="img4" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/FAQ.png" alt="Question" />
 	      	<img class="answer_img" id="img5" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/Check.png" alt="Bonne réponse" />
 	      	<img class="answer_img" id="img6" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/Check_wrong.png" alt="Mauvaise réponse" />
 	      </div>
+	      <div class="col-md-12 choix_joker" id="choix_joker_02">
+	      	<div class="col-md-4">
+	      		<a href="#" title="Cash" id="cash_02" onclick="decompte();">Cash</a>
+	      	</div>
+	      	<div class="col-md-4">
+	      		<a href="#" title="Flash" id="flash_02">Flash</a>
+	      	</div>
+	      	<div class="col-md-4">
+	      		<a href="#" title="Bingo" id="bingo_02">Bingo</a>
+	      	</div>
+	      </div>
 	      <div id="answer2" class="col-md-12 answers text-center">
-	      	<p class="reponse" id="reponse2">La réponse rédigée</p>
+	      	<p class="reponse" id="reponse2i">La réponse rédigée pour Flash</p>
+	      	<p class="reponse" id="reponse2">La réponse rédigée pour Bingo</p>
 	      </div>
 	      <div id="next2" class="col-md-12 text-center next">
 	      	<a href="#" class="button_answer" id="n2">Suivant</a>
@@ -57,19 +95,37 @@
 	    <div class="question col-md-12">
 	      <div class="texte_container col-md-5">
 	        <h2>Question 3</h2>
+	          <div id="question_simple_03">Question plus simple</div>
 	          <p>Intitulé de la question :</p>
-	          <p><input type="radio" id="r7" name="q1">Réponse 1 (fausse)</p>
-	          <p><input type="radio" id="r8" name="q1">Réponse 2 (fausse)</p>
-	          <p><input type="radio" id="r9" name="q1">Réponse 3 (vraie)</p>
+	          <p><input type="radio" id="r7" name="q3">Réponse 1 (fausse)</p>
+	          <p><input type="radio" id="r8" name="q3">Réponse 2 (fausse)</p>
+	          <p><input type="radio" id="r9" name="q3">Réponse 3 (vraie)</p>
+	          <p><input type="radio" id="i7" name="q3">Texte (fausse)</p>
+	          <p><input type="radio" id="i8" name="q3">Texte (fausse)</p>
+	          <p><input type="radio" id="i9" name="q3">Intrus (vraie)</p>
+	          <div id="indice_03" class="indices">Indice</div>
 	          <a href="#" class="button_answer" id="a3">Confirmer</a>
+	          <a href="#" class="button_answer" id="ai3">Confirmer</a>
 	      </div>
 	      <div class="col-md-7 answer_img_container">
 	      	<img class="question_img" id="img7" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/FAQ.png" alt="Question" />
 	      	<img class="answer_img" id="img8" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/Check.png" alt="Bonne réponse" />
 	      	<img class="answer_img" id="img9" src="<?php bloginfo('url'); ?>/wp-content/themes/Questionnaire_SG/img/Check_wrong.png" alt="Mauvaise réponse" />
 	      </div>
+	      <div class="col-md-12 choix_joker" id="choix_joker_03">
+	      	<div class="col-md-4">
+	      		<a href="#" title="Cash" id="cash_32" onclick="decompte();">Cash</a>
+	      	</div>
+	      	<div class="col-md-4">
+	      		<a href="#" title="Flash" id="flash_03">Flash</a>
+	      	</div>
+	      	<div class="col-md-4">
+	      		<a href="#" title="Bingo" id="bingo_03">Bingo</a>
+	      	</div>
+	      </div>
 	      <div id="answer3" class="col-md-12 answers text-center">
-	      	<p class="reponse" id="reponse1">La réponse rédigée</p>
+	      <p class="reponse" id="reponse3i">La réponse rédigée pour Flash</p>
+	      	<p class="reponse" id="reponse3">La réponse rédigée pour Bingo</p>
 	      </div>
 	      <div id="next3" class="col-md-12 text-center next">
 	      	<a href="#" class="button_answer" id="n3"><button type="submit">Suivant</button></a>
--- wp-content/themes/Questionnaire_SG/js/my_js.js
+++ wp-content/themes/Questionnaire_SG/js/my_js.js
@@ -4,6 +4,11 @@ $(document).ready(function()
 {
 
   //Questionnaire
+  $("#question1 .question .texte_container p:gt(0)").hide();
+  $("#indice_01").hide();
+  $("#question_simple_01").hide();
+  $("#a1").hide();
+  $("#ai1").hide();
   $("#question2").hide();
   $("#question3").hide();
   $(".answers").hide();
@@ -14,15 +19,127 @@ $(document).ready(function()
    
 });
 
-$('#a1').click(
+//Vérification erreur ou non flash
+var error_on_flash = 0;
+
+//Choix Flash
+$("#flash_01").click(function(){
+  $("#choix_joker_01").hide();
+  $("#question1 .question .texte_container p:gt(3)").show(200);
+  $("#question1 .question .texte_container p:lt(3)").hide();
+  $("#ai1").show(200);
+  $("#indice_01").show(200);
+});
+$("#flash_02").click(function(){
+  $("#choix_joker_02").hide();
+  $("#question2 .question .texte_container p:gt(3)").show(200);
+  $("#question2 .question .texte_container p:lt(3)").hide();
+  $("#ai2").show(200);
+  if(error_on_flash == 0)
+  {
+    $("#indice_02").show(200);
+  }
+  else
+  {
+    $("#indice_02").hide();
+  }
+});
+$("#flash_03").click(function(){
+  $("#choix_joker_03").hide();
+  $("#question3 .question .texte_container p:gt(3)").show(200);
+  $("#question3 .question .texte_container p:lt(3)").hide();
+  $("#ai3").show(200);
+  if(error_on_flash == 0)
+  {
+    $("#indice_03").show(200);
+  }
+  else
+  {
+    $("#indice_03").hide();
+  }
+});
+
+//Choix joker
+$("#bingo_01").click(function(){
+  $("#choix_joker_01").hide();
+  $("#question1 .question .texte_container p:first").hide();
+  $("#question_simple_01").show(200);
+  $("#question1 .question .texte_container p:gt(0)").show(200);
+  $("#question1 .question .texte_container p:gt(3)").hide();
+  $("#a1").show(200);
+});
+$("#bingo_02").click(function(){
+  $("#choix_joker_02").hide();
+  $("#question2 .question .texte_container p:first").hide();
+  $("#question_simple_02").show(200);
+  $("#question2 .question .texte_container p:gt(0)").show(200);
+  $("#question2 .question .texte_container p:gt(3)").hide();
+  $("#a2").show(200);
+});
+$("#bingo_03").click(function(){
+  $("#choix_joker_03").hide();
+  $("#question3 .question .texte_container p:first").hide();
+  $("#question_simple_03").show(200);
+  $("#question3 .question .texte_container p:gt(0)").show(200);
+  $("#question3 .question .texte_container p:gt(3)").hide();
+  $("#a3").show(200);
+});
+
+//Initialisation score
+var score = 0;
+var txt_score = " point.";
+document.getElementById("score").innerHTML = score + txt_score;
+function updatescore() 
+{
+    if((score >= -1) && (score <= 1))
+    {
+      txt_score = " point."
+    }
+    else
+    {
+      txt_score = " points."
+    }
+    document.getElementById("score").innerHTML = score + txt_score;
+}
+
+//Compteur
+var cpt = 15 ;
+var x;
+ 
+function decompte()
+{
+    if(cpt>=0)
+    {
+        if(cpt>1)
+        {
+            var sec = " secondes.";
+        } else {
+            var sec = " seconde.";
+        }
+        document.getElementById("Crono").innerHTML = "Vous n'avez plus que " + cpt + sec ;
+        cpt-- ;
+        x = setTimeout("decompte()",1000) ;
+    }
+    else
+    {
+        clearTimeout(x) ;
+    }
+}
+
+//Confirmation réponses Flash
+$('#ai1').click(
           function() { 
             $('.answers').show(200);
-            if ($(':radio[id="r1"]:checked').val()) {
+            $('#reponse1').hide();
+            if ($(':radio[id="i1"]:checked').val()) {
               $('#img1').hide();
               $('#img2').show(200);
               $('#img3').hide();
               $('#answer1').css('background', '#0F0');
               $('.reponse').css('color', '#FFF');
+              score = score+3;
+              updatescore();
+              error_on_flash = 0;
             }  
             else{
               $('#img1').hide();
@@ -30,26 +147,24 @@ $('#a1').click(
               $('#img3').show(200);
               $('#answer1').css('background', '#F00');
               $('.reponse').css('color', '#FFF');
+              error_on_flash = 1;
             }
             $('#next1').show(200);
-            $('#a1').hide();
+            $('#ai1').hide();
           });
-
-   $('#n1').click(
-          function() {
-            $('#question1').hide();
-            $('#question2').show(200);
-          });
-
-   $('#a2').click(
+$('#ai2').click(
           function() { 
             $('.answers').show(200);
-            if ($(':radio[id="r5"]:checked').val()) {
+            $('#reponse2').hide();
+            if ($(':radio[id="i5"]:checked').val()) {
               $('#img4').hide();
               $('#img5').show(200);
               $('#img6').hide();
               $('#answer2').css('background', '#0F0');
               $('.reponse').css('color', '#FFF');
+              score = score+3;
+              updatescore();
+              error_on_flash = 0;
             }  
             else{
               $('#img4').hide();
@@ -57,34 +172,136 @@ $('#a1').click(
               $('#img6').show(200);
               $('#answer2').css('background', '#F00');
               $('.reponse').css('color', '#FFF');
+              error_on_flash = 1;
+            }
+            $('#next2').show(200);
+            $('#ai2').hide();
+          });
+$('#ai3').click(
+          function() { 
+            $('.answers').show(200);
+            $('#reponse3').hide();
+            if ($(':radio[id="i9"]:checked').val()) {
+              $('#img4').hide();
+              $('#img5').show(200);
+              $('#img6').hide();
+              $('#answer3').css('background', '#0F0');
+              $('.reponse').css('color', '#FFF');
+              score = score+3;
+              updatescore();
+              error_on_flash = 0;
+            }  
+            else{
+              $('#img4').hide();
+              $('#img5').hide();
+              $('#img6').show(200);
+              $('#answer3').css('background', '#F00');
+              $('.reponse').css('color', '#FFF');
+              error_on_flash = 1;
+            }
+            $('#next3').show(200);
+            $('#ai3').hide();
+          });
+
+//Confirmation réponses Bingo
+$('#a1').click(
+          function() { 
+            $('.answers').show(200);
+            $('#reponse1i').hide();
+            if ($(':radio[id="r1"]:checked').val()) {
+              $('#img1').hide();
+              $('#img2').show(200);
+              $('#img3').hide();
+              $('#answer1').css('background', '#0F0');
+              $('.reponse').css('color', '#FFF');
+              score = score+1;
+              updatescore();
+            }  
+            else{
+              $('#img1').hide();
+              $('#img2').hide();
+              $('#img3').show(200);
+              $('#answer1').css('background', '#F00');
+              $('.reponse').css('color', '#FFF');
+              score = score-1;
+              updatescore();
+            }
+            $('#next1').show(200);
+            $('#a1').hide();
+          });
+
+   $('#a2').click(
+          function() { 
+            $('.answers').show(200);
+             $('#reponse2i').hide();
+            if ($(':radio[id="r5"]:checked').val()) {
+              $('#img4').hide();
+              $('#img5').show(200);
+              $('#img6').hide();
+              $('#answer2').css('background', '#0F0');
+              $('.reponse').css('color', '#FFF');
+              score = score+1;
+              updatescore();
+            }  
+            else{
+              $('#img4').hide();
+              $('#img5').hide();
+              $('#img6').show(200);
+              $('#answer2').css('background', '#F00');
+              $('.reponse').css('color', '#FFF');
+              score = score-1;
+              updatescore();
             }
             $('#next2').show(200);
             $('#a2').hide();
           });
 
-    $('#n2').click(
-          function() {
-            $('#question2').hide();
-            $('#question3').show(200);
-          });
-
     $('#a3').click(
           function() { 
             $('.answers').show(200);
+            $('#reponse3i').hide();
             if ($(':radio[id="r9"]:checked').val()) {
               $('#img7').hide();
               $('#img8').show(200);
               $('#img9').hide();
               $('#answer3').css('background', '#0F0');
               $('.reponse').css('color', '#FFF');
+              score = score+1;
+              updatescore();
             }  
             else{
               $('#img7').hide();
               $('#img8').hide();
               $('#img9').show(200);
-              $('#answer2').css('background', '#F00');
+              $('#answer3').css('background', '#F00');
               $('.reponse').css('color', '#FFF');
+              score = score-1;
+              updatescore();
             }
             $('#next3').show(200);
             $('#a3').hide();
+          });
+
+//Boutons "Suivant"
+
+$('#n1').click(
+          function() {
+            $('#question1').hide();
+            $("#question2 .question .texte_container p:gt(0)").hide();
+            $("#a2").hide();
+            $("#ai2").hide();
+            $("#question2").show(200);
+            $("#indice_02").hide();
+            $("#question_simple_02").hide();
+          });
+
+    $('#n2').click(
+          function() {
+            $('#question2').hide();
+            $("#question3 .question .texte_container p:gt(0)").hide();
+            $("#a3").hide();
+            $("#ai3").hide();
+            $("#question3").show(200);
+            $("#indice_03").hide();
+            $("#question_simple_03").hide();
           });
