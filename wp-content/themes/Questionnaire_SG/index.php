74d13908ea8d6a142633bc374f5958fccc8e676c
Kévin Huang <Kvin Huang>
2016-11-24 23:56:43 +0100

    Jeu entièrement terminé, juste une question à ajouter pour bingo et optimisation du compteur

--- wp-content/themes/Questionnaire_SG/index.php
+++ wp-content/themes/Questionnaire_SG/index.php
@@ -12,7 +12,11 @@
 	        <h2>Question 1</h2>
 	          <div id="question_simple_01">Question plus simple</div>
 	          <p>Intitulé de la question :</p>
-	          <div id="Crono"></div>
+	          <div id="cash_container_01">
+		          <div id="Crono_01"></div>
+		          <div id="ReponseCash_01" class="cash"><input type="text" id="reponse_cash_01" /></div>
+		          <a href="#" class="button_answer" id="ac1">Confirmer</a>
+	          </div>
 	          <p><input type="radio" id="r1" name="q1">Réponse 1 (vraie)</p>
 	          <p><input type="radio" id="r2" name="q1">Réponse 2 (fausse)</p>
 	          <p><input type="radio" id="r3" name="q1">Réponse 3 (fausse)</p>
@@ -30,7 +34,7 @@
 	      </div>
 	      <div class="col-md-12 choix_joker" id="choix_joker_01">
 	      	<div class="col-md-4">
-	      		<a href="#" title="Cash" id="cash_01" onclick="decompte();">Cash</a>
+	      		<a href="#" title="Cash" id="cash_01" onclick="decompte01();">Cash</a>
 	      	</div>
 	      	<div class="col-md-4">
 	      		<a href="#" title="Flash" id="flash_01">Flash</a>
@@ -40,6 +44,7 @@
 	      	</div>
 	      </div>
 	      <div id="answer1" class="col-md-12 answers text-center">
+	      	<p class="reponse" id="reponse1c">La réponse rédigée pour Cash</p>
 	      	<p class="reponse" id="reponse1i">La réponse rédigée pour Flash</p>
 	      	<p class="reponse" id="reponse1">La réponse rédigée pour Bingo</p>
 	      </div>
@@ -55,6 +60,11 @@
 	        <h2>Question 2</h2>
 	          <div id="question_simple_02">Question plus simple</div>
 	          <p>Intitulé de la question :</p>
+	          <div id="cash_container_02">
+		          <div id="Crono_02"></div>
+		          <div id="ReponseCash_02" class="cash"><input type="text" id="reponse_cash_02" /></div>
+		          <a href="#" class="button_answer" id="ac2">Confirmer</a>
+	          </div>
 	          <p><input type="radio" id="r4" name="q2">Réponse 1 (fausse)</p>
 	          <p><input type="radio" id="r5" name="q2">Réponse 2 (vraie)</p>
 	          <p><input type="radio" id="r6" name="q2">Réponse 3 (fausse)</p>
@@ -72,7 +82,7 @@
 	      </div>
 	      <div class="col-md-12 choix_joker" id="choix_joker_02">
 	      	<div class="col-md-4">
-	      		<a href="#" title="Cash" id="cash_02" onclick="decompte();">Cash</a>
+	      		<a href="#" title="Cash" id="cash_02" onclick="decompte02();">Cash</a>
 	      	</div>
 	      	<div class="col-md-4">
 	      		<a href="#" title="Flash" id="flash_02">Flash</a>
@@ -82,6 +92,7 @@
 	      	</div>
 	      </div>
 	      <div id="answer2" class="col-md-12 answers text-center">
+	      	<p class="reponse" id="reponse2c">La réponse rédigée pour Cash</p>
 	      	<p class="reponse" id="reponse2i">La réponse rédigée pour Flash</p>
 	      	<p class="reponse" id="reponse2">La réponse rédigée pour Bingo</p>
 	      </div>
@@ -97,6 +108,11 @@
 	        <h2>Question 3</h2>
 	          <div id="question_simple_03">Question plus simple</div>
 	          <p>Intitulé de la question :</p>
+	          <div id="cash_container_03">
+		          <div id="Crono_03"></div>
+		          <div id="ReponseCash_03" class="cash"><input type="text" id="reponse_cash_03" /></div>
+		          <a href="#" class="button_answer" id="ac3">Confirmer</a>
+	          </div>
 	          <p><input type="radio" id="r7" name="q3">Réponse 1 (fausse)</p>
 	          <p><input type="radio" id="r8" name="q3">Réponse 2 (fausse)</p>
 	          <p><input type="radio" id="r9" name="q3">Réponse 3 (vraie)</p>
@@ -114,7 +130,7 @@
 	      </div>
 	      <div class="col-md-12 choix_joker" id="choix_joker_03">
 	      	<div class="col-md-4">
-	      		<a href="#" title="Cash" id="cash_32" onclick="decompte();">Cash</a>
+	      		<a href="#" title="Cash" id="cash_03" onclick="decompte03();">Cash</a>
 	      	</div>
 	      	<div class="col-md-4">
 	      		<a href="#" title="Flash" id="flash_03">Flash</a>
@@ -124,6 +140,7 @@
 	      	</div>
 	      </div>
 	      <div id="answer3" class="col-md-12 answers text-center">
+	      <p class="reponse" id="reponse3c">La réponse rédigée pour Cash</p>
 	      <p class="reponse" id="reponse3i">La réponse rédigée pour Flash</p>
 	      	<p class="reponse" id="reponse3">La réponse rédigée pour Bingo</p>
 	      </div>
--- wp-content/themes/Questionnaire_SG/js/my_js.js
+++ wp-content/themes/Questionnaire_SG/js/my_js.js
@@ -7,6 +7,7 @@ $(document).ready(function()
   $("#question1 .question .texte_container p:gt(0)").hide();
   $("#indice_01").hide();
   $("#question_simple_01").hide();
+  $("#cash_container_01").hide();
   $("#a1").hide();
   $("#ai1").hide();
   $("#question2").hide();
@@ -17,6 +18,88 @@ $(document).ready(function()
 
 
    
+});
+
+//Compteur pour Cash
+var cpt = 15 ; var cpt2 = 15; var cpt3 = 15;
+var x,y,z;
+ 
+function decompte01()
+{
+    if(cpt>=0)
+    {
+        if(cpt>1)
+        {
+            var sec = " secondes.";
+        } else {
+            var sec = " seconde.";
+        }
+        document.getElementById("Crono_01").innerHTML = "Vous n'avez plus que " + cpt + sec ;
+        cpt-- ;
+        x = setTimeout("decompte01()",1000) ;
+        return cpt;
+    }
+    else
+    {
+        clearTimeout(x);
+    }
+}
+function decompte02()
+{
+    if(cpt2>=0)
+    {
+        if(cpt2>1)
+        {
+            var sec = " secondes.";
+        } else {
+            var sec = " seconde.";
+        }
+        document.getElementById("Crono_02").innerHTML = "Vous n'avez plus que " + cpt2 + sec ;
+        cpt2-- ;
+        y = setTimeout("decompte02()",1000) ;
+        return cpt2;
+    }
+    else
+    {
+        clearTimeout(y);
+    }
+}
+function decompte03()
+{
+    if(cpt3>=0)
+    {
+        if(cpt3>1)
+        {
+            var sec = " secondes.";
+        } else {
+            var sec = " seconde.";
+        }
+        document.getElementById("Crono_03").innerHTML = "Vous n'avez plus que " + cpt3 + sec ;
+        cpt3-- ;
+        z = setTimeout("decompte03()",1000) ;
+        return cpt3;
+    }
+    else
+    {
+        clearTimeout(z);
+    }
+}
+
+//Vérification erreur ou non cash
+var error_on_cash = 0;
+
+//Choix Cash
+$("#cash_01").click(function(){
+  $("#cash_container_01").show(200);
+  $("#choix_joker_01").hide();
+});
+$("#cash_02").click(function(){
+  $("#cash_container_02").show(200);
+  $("#choix_joker_02").hide();
+});
+$("#cash_03").click(function(){
+  $("#cash_container_03").show(200);
+  $("#choix_joker_03").hide();
 });
 
 //Vérification erreur ou non flash
@@ -102,35 +185,100 @@ function updatescore()
     document.getElementById("score").innerHTML = score + txt_score;
 }
 
-//Compteur
-var cpt = 15 ;
-var x;
- 
-function decompte()
-{
-    if(cpt>=0)
-    {
-        if(cpt>1)
-        {
-            var sec = " secondes.";
-        } else {
-            var sec = " seconde.";
-        }
-        document.getElementById("Crono").innerHTML = "Vous n'avez plus que " + cpt + sec ;
-        cpt-- ;
-        x = setTimeout("decompte()",1000) ;
-    }
-    else
-    {
-        clearTimeout(x) ;
-    }
-}
+//Confirmation réponses Cash
+$("#ac1").click(function(){
+  var cash_value_01 = $('#reponse_cash_01').val(); //Vérification réponses Cash
+  $('.answers').show(200);
+            $('#reponse1').hide();
+            $('#reponse1i').hide();
+            if ((cpt>0) && (cash_value_01 == "La réponse")) {
+              cpt = 0;
+              $('#img1').hide();
+              $('#img2').show(200);
+              $('#img3').hide();
+              $('#answer1').css('background', '#0F0');
+              $('.reponse').css('color', '#FFF');
+              score = score+5;
+              updatescore();
+              error_on_cash = 0;
+            }  
+            else{
+              cpt = 0;
+              $('#img1').hide();
+              $('#img2').hide();
+              $('#img3').show(200);
+              $('#answer1').css('background', '#F00');
+              $('.reponse').css('color', '#FFF');
+              error_on_cash = 1;
+            }
+            $('#next1').show(200);
+            $('#ac1').hide();
+            $("#cash_container_02").hide();
+});
+$("#ac2").click(function(){
+  var cash_value_02 = $('#reponse_cash_02').val(); //Vérification réponses Cash
+  $('.answers').show(200);
+            $('#reponse2').hide();
+            $('#reponse2i').hide();
+            if ((cpt2>0) && (cash_value_02 == "La réponse")) {
+              cpt2 = 0;
+              $('#img1').hide();
+              $('#img2').show(200);
+              $('#img3').hide();
+              $('#answer2').css('background', '#0F0');
+              $('.reponse').css('color', '#FFF');
+              score = score+5;
+              updatescore();
+              error_on_cash = 0;
+            }  
+            else{
+              cpt2 = 0;
+              $('#img1').hide();
+              $('#img2').hide();
+              $('#img3').show(200);
+              $('#answer2').css('background', '#F00');
+              $('.reponse').css('color', '#FFF');
+              error_on_cash = 1;
+            }
+            $('#next2').show(200);
+            $('#ac2').hide();
+            $("#cash_container_03").hide();
+});
+$("#ac3").click(function(){
+  var cash_value_03 = $('#reponse_cash_03').val(); //Vérification réponses Cash
+  $('.answers').show(200);
+            $('#reponse3').hide();
+            $('#reponse3i').hide();
+            if ((cpt3>0) && (cash_value_03 == "La réponse")) {
+              cpt3 = 0;
+              $('#img1').hide();
+              $('#img2').show(200);
+              $('#img3').hide();
+              $('#answer3').css('background', '#0F0');
+              $('.reponse').css('color', '#FFF');
+              score = score+5;
+              updatescore();
+              error_on_cash = 0;
+            }  
+            else{
+              cpt3 = 0;
+              $('#img1').hide();
+              $('#img2').hide();
+              $('#img3').show(200);
+              $('#answer3').css('background', '#F00');
+              $('.reponse').css('color', '#FFF');
+              error_on_cash = 1;
+            }
+            $('#next3').show(200);
+            $('#ac3').hide();
+});
 
 //Confirmation réponses Flash
 $('#ai1').click(
           function() { 
             $('.answers').show(200);
             $('#reponse1').hide();
+            $('#reponse1c').hide();
             if ($(':radio[id="i1"]:checked').val()) {
               $('#img1').hide();
               $('#img2').show(200);
@@ -156,6 +304,7 @@ $('#ai2').click(
           function() { 
             $('.answers').show(200);
             $('#reponse2').hide();
+            $('#reponse2c').hide();
             if ($(':radio[id="i5"]:checked').val()) {
               $('#img4').hide();
               $('#img5').show(200);
@@ -181,6 +330,7 @@ $('#ai3').click(
           function() { 
             $('.answers').show(200);
             $('#reponse3').hide();
+            $('#reponse3c').hide();
             if ($(':radio[id="i9"]:checked').val()) {
               $('#img4').hide();
               $('#img5').show(200);
@@ -208,6 +358,7 @@ $('#a1').click(
           function() { 
             $('.answers').show(200);
             $('#reponse1i').hide();
+            $('#reponse1c').hide();
             if ($(':radio[id="r1"]:checked').val()) {
               $('#img1').hide();
               $('#img2').show(200);
@@ -234,6 +385,7 @@ $('#a1').click(
           function() { 
             $('.answers').show(200);
              $('#reponse2i').hide();
+             $('#reponse2c').hide();
             if ($(':radio[id="r5"]:checked').val()) {
               $('#img4').hide();
               $('#img5').show(200);
@@ -260,6 +412,7 @@ $('#a1').click(
           function() { 
             $('.answers').show(200);
             $('#reponse3i').hide();
+            $('#reponse3c').hide();
             if ($(':radio[id="r9"]:checked').val()) {
               $('#img7').hide();
               $('#img8').show(200);
@@ -283,25 +436,44 @@ $('#a1').click(
           });
 
 //Boutons "Suivant"
-
 $('#n1').click(
           function() {
-            $('#question1').hide();
+            $("#question1").hide();
             $("#question2 .question .texte_container p:gt(0)").hide();
             $("#a2").hide();
             $("#ai2").hide();
             $("#question2").show(200);
             $("#indice_02").hide();
             $("#question_simple_02").hide();
+            $("#cash_container_02").hide();
+            if(error_on_cash == 0)
+            {
+              $("#cash_02").show(200);
+            }
+            else
+            {
+              $("#cash_02").hide();
+              error_on_cash = 0;
+            }
           });
 
     $('#n2').click(
           function() {
-            $('#question2').hide();
+            $("#question2").hide();
             $("#question3 .question .texte_container p:gt(0)").hide();
             $("#a3").hide();
             $("#ai3").hide();
             $("#question3").show(200);
             $("#indice_03").hide();
             $("#question_simple_03").hide();
+            $("#cash_container_03").hide();
+            if(error_on_cash == 0)
+            {
+              $("#cash_03").show(200);
+            }
+            else
+            {
+              $("#cash_03").hide();
+              error_on_cash = 0;
+            }
           });
